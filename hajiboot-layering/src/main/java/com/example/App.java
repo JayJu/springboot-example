f8ae580c567d5d982249e3a695bdccde3b6fb645
1015331 <iam@juheejin.com>
2016-11-14 16:24:36 +0900

    2.2.1 JdbcTemplate 으로 접속하기

--- hajiboot-layering/src/main/java/com/example/App.java
+++ hajiboot-layering/src/main/java/com/example/App.java
@@ -7,9 +7,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by 1015331 on 2016-11-11.
@@ -23,9 +27,15 @@ public class App implements CommandLineRunner{
 
     @Override
     public void run(String... strings) throws Exception {
-        String sql = "SELECT 1";
-        SqlParameterSource param = new MapSqlParameterSource();
-        Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+        String sql = "SELECT id, first_name, last_name FROM  customers WHERE id = :id";
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("id", 1);
+        Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
+            @Override
+            public Customer mapRow(ResultSet resultSet, int i) throws SQLException {
+                return new Customer(resultSet.getInt("id"), resultSet.getString("first_name"), resultSet.getString("last_name"));
+            }
+        });
 
         System.out.println("result: " + result);
     }
--- hajiboot-layering/src/main/java/com/example/domain/Customer.java
+++ hajiboot-layering/src/main/java/com/example/domain/Customer.java
@@ -3,6 +3,7 @@ package com.example.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by 1015331 on 2016-11-11.
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Component
 public class Customer {
     private Integer id;
     private String firstName;
