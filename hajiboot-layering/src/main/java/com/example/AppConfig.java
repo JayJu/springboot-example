03f2450142764cc6a1e0b833027ee5d57a9b6a18
1015331 <iam@juheejin.com>
2016-11-14 18:15:56 +0900

    2.2.1 JdbcTemplate 으로 접속하기  - Log4JDBC 로 SQL 로그 출력하기

---
+++ hajiboot-layering/src/main/java/com/example/AppConfig.java
@@ -0,0 +1,41 @@
+package com.example;
+
+
+import javax.sql.DataSource;
+
+
+import net.sf.log4jdbc.Log4jdbcProxyDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * Created by 1015331 on 2016-11-14.
+ */
+@Configuration
+public class AppConfig {
+    @Autowired
+    DataSourceProperties dataSourceProperties;
+    DataSource dataSource;
+
+    @Bean
+    DataSource realDataSource() {
+        DataSourceBuilder factory = DataSourceBuilder
+                .create(this.dataSourceProperties.getClassLoader())
+                .url(this.dataSourceProperties.getUrl())
+                .username(this.dataSourceProperties.getUsername())
+                .password(this.dataSourceProperties.getPassword());
+        this.dataSource = factory.build();
+
+        return this.dataSource;
+    }
+
+    @Bean
+    @Primary
+    DataSource dataSource() {
+        return new Log4jdbcProxyDataSource(this.dataSource);
+    }
+}
