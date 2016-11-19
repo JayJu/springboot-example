ce7e955ac76346b25e3584e4ad45458f36e31431
1015331 <iam@juheejin.com>
2016-11-19 10:56:59 +0900

    3장. 스프링 부트로 웹 어플리케이션 개발하기 3.2 REST 웹 서비스 개발

--- hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
+++ hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
@@ -3,6 +3,9 @@ package com.example.api;
 import com.example.domain.Customer;
 import com.example.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,21 +25,15 @@ public class CustomerRestController {
     CustomerService customerService;
 
     @RequestMapping(method = RequestMethod.GET)
-    List<Customer> getCustomers() {
-        return customerService.findAll();
+    Page<Customer> getCustomers(@PageableDefault Pageable pageable) {
+        Page<Customer> customers = customerService.findAll(pageable);
+        return customers;
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     Customer getCustomer(@PathVariable Integer id) {
         return customerService.findOne(id);
     }
-
-/*    //신규고객
-    @RequestMapping(method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.CREATED)
-    Customer postCustomers(@RequestBody Customer customer) {
-        return customerService.create(customer);
-    }*/
 
     //신규고객
     @RequestMapping(method = RequestMethod.POST)
